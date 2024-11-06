b5211942db0cb18d5ef0f8a14e3798e1d026f8b7
kodesayapro <arifnurrahman6789@gmail.com>
2024-11-06 09:52:47 +0700

    commit pertama bin2dec

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "name": "bin2dec-v1",
+  "start_url": ".",
+  "display": "standalone",
+  "theme_color": "#f8fcfa",
+  "background_color": "#f8fcfa",
+  "icons": [
+    {
+      "src": "icons/user192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "icons/user512.png",
+      "type": "image/png",
+      "sizes": "512x512",
+      "purpose": "maskable"
+    }
+  ]
+}
